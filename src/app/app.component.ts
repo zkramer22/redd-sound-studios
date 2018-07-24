5049c828524b0798c70bf746922156073c694682
zach kramer <zkramer22@gmail.com>
2018-07-24 03:21:15 -0400

    responsive styling for mobile, hamburger menu for mobile and small desktop screen width.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,37 @@ export class AppComponent {
 
   ngOnInit() {
     $(document).ready(() => {
+      const $hamburger = $('#hamburger');
+      const $hamburgerClose = $('#hamburger-close');
+      const $hamburgerLinks = $('#hamburger-links');
+
       $(document).scroll(() => {
         this.scrollPos = $(window).scrollTop();
         if (this.scrollButton === false && this.scrollPos >= 300) {
           this.scrollButton = true;
-          $('#scroll-to-top').animate({ bottom: '0' }, 200);
+          $('#scroll-to-top').animate({ bottom: '0' }, 160);
         }
         else if (this.scrollButton === true && this.scrollPos < 300) {
           this.scrollButton = false;
           $('#scroll-to-top').animate({ bottom: '-200px' }, 200);
         }
       });
+
+      $hamburger.click(() => {
+        $hamburgerLinks.animate({ right: '0' }, 300);
+        $hamburger.fadeOut(200);
+        $hamburgerClose.fadeIn(200);
+
+        $hamburgerClose.click((e) => {
+          console.log(e.target);
+          $hamburgerLinks.animate({ right: '-320px' }, 300);
+          $hamburgerClose.fadeOut(200);
+          $hamburger.fadeIn(200);
+        });
+      });
+
     });
 
-    
   }
 
   scrollToTop() {
